d4689faa474eaa9507cf4d269b3658fcf07098b6
Capelaum <thecapellett@gmail.com>
2021-01-22 20:58:15 -0300

    netlify plugin config

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,12 +1,13 @@
-import Link from 'next/Link';
+import Link from "next/Link";
 
 export default function Footer() {
     return (
-    <footer className="footer">
-    <Link href="https://github.com/capelaum" target="_blank">
-        <a target="_blank"><i class="fab fa-github"></i>  Capela MasterChef</a>
-    </Link>
-        
-    </footer>
+        <footer className="footer">
+            <Link href="https://github.com/capelaum" target="_blank">
+            <a target="_blank">
+                <i class="fab fa-github"></i> Capela MasterChef
+            </a>
+            </Link>
+        </footer>
     );
 }
